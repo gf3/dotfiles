2bd53e53d5fc93292c3702fcc9320c80eea78b17
Gianni Chiappetta <gianni@runlevel6.org>
2013-03-27 10:22:13 -0700

    Mutt mail config, mostly copied from @sjl
    * Relies on Keychain access on OS X
    * See: http://stevelosh.com/blog/2012/10/the-homely-mutt

---
+++ .mutt/offlineimap.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import re, subprocess
+def get_keychain_pass(account=None, server=None):
+    params = {
+        'security': '/usr/bin/security',
+        'command': 'find-internet-password',
+        'account': account,
+        'server': server,
+        'keychain': '/Users/`whoami`/Library/Keychains/login.keychain',
+    }
+    command = "sudo -u `whoami` %(security)s -v %(command)s -g -a %(account)s -s %(server)s %(keychain)s" % params
+    output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    outtext = [l for l in output.splitlines()
+               if l.startswith('password: ')][0]
+
+    return re.match(r'password: "(.*)"', outtext).group(1)
