6b2194b56cc247cf57ba05b7a45eb9a600634823
Gianni Chiappetta <gianni@runlevel6.org>
2014-07-04 18:15:40 -0400

    Remove a whole bunch of shit I either don't use or don't want to maintain

--- .scripts/generate-clojure-completions.clj
+++
@@ -1,4 +0,0 @@
-(def completions (mapcat (comp keys ns-publics) (all-ns)))
- 
-(with-open [f (java.io.BufferedWriter. (java.io.FileWriter. (str (System/getenv "HOME") "/.clj_completions")))]
-  (.write f (apply str (interpose \newline completions))))
