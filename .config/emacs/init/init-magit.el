26f01926596806144e2aeed4ce9805d2741092da
Gianni Chiappetta <gianni@runlevel6.org>
2023-05-23 16:05:00 -0400

    fix(emacs): fix forge loading

--- .config/emacs/init/init-magit.el
+++ .config/emacs/init/init-magit.el
@@ -2,14 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(with-eval-after-load 'magit
-  (progn
-	(require 'forge)
-	(transient-append-suffix 'forge-dispatch '(0)
-	  ["Edit"
-	   ("e a" "assignees" forge-edit-topic-assignees)
-	   ("e r" "review requests" forge-edit-topic-review-requests)])))
-
 (use-package magit
   :straight (:host github :repo "magit/magit")
   :demand t
@@ -18,6 +10,16 @@
   (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
   (setq magit-define-global-key-bindings t)
   (transient-suffix-put 'magit-dispatch "k" :key "x"))
+
+(use-package forge
+  :straight t
+  :after magit
+  :demand t
+  :config
+  (transient-append-suffix 'forge-dispatch '(0)
+	["Edit"
+	 ("e a" "assignees" forge-edit-topic-assignees)
+	 ("e r" "review requests" forge-edit-topic-review-requests)]))
 
 (provide 'init-magit)
 ;;; init-magit.el ends here
