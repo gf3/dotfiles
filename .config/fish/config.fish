d78f05691b30db8c53b761e406ce75bb0ae6bf7a
Gianni Chiappetta <gianni@runlevel6.org>
2025-12-23 14:33:41 -0500

    feat(fish): misc. small changes, swap exa for lsd

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,25 +1,28 @@
 status is-login; and begin
-# Login shell initialisation
+    # Login shell initialisation
 end
 
 status is-interactive; and begin
-# Abbreviations
-abbr --add -- b bat
-abbr --add -- g git
+    # Abbreviations
+    abbr --add -- b bat
+    abbr --add -- g git
+    abbr --add -- k kubectl
 
-# Aliases
-alias ... 'cd ../..'
-alias l 'eza --icons --git'
-alias ll 'eza --icons --long --header --group --created --modified --git -a'
+    # Aliases
+    alias ... 'cd ../..'
+    alias .... 'cd ../../..'
+    alias ..... 'cd ../../../..'
+    alias l 'lsd -h --group-dirs=first --git'
+    alias ll 'lsd --long -h --group-dirs=first --git -A'
 
-# Interactive shell initialisation
-set -gx GPG_TTY (tty)
-set -gx EDITOR "emacs -nw"
-set -gx ELIXIR_ERL_OPTIONS "-kernel shell_history enabled"
-# set -gx SSH_AUTH_SOCK "~/.bitwarden-ssh-agent.sock"
-set -gx SSH_AUTH_SOCK ~/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
+    # Interactive shell initialisation
+    set -gx GPG_TTY (tty)
+    set -gx EDITOR nano
+    set -gx ELIXIR_ERL_OPTIONS "-kernel shell_history enabled"
+    # set -gx SSH_AUTH_SOCK "~/.bitwarden-ssh-agent.sock"
+    set -gx SSH_AUTH_SOCK ~/.var/app/com.bitwarden.desktop/data/.bitwarden-ssh-agent.sock
 
-# Tools
-direnv hook fish | source
-zoxide init fish | source
+    # Tools
+    direnv hook fish | source
+    zoxide init fish | source
 end
